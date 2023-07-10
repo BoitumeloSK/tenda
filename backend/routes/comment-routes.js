c1c218ca00f747d2efa7e860997a572d1e180429
boitumelosk <boitumelo75@gmail.com>
2023-07-10 13:17:15 +0200

    Removed frontend folder

---
+++ backend/routes/comment-routes.js
@@ -0,0 +1,4 @@
+const  {addComment,deleteComment} = require("../controllers/comment-controller")
+const {auth} = require("../middleware/auth")
+const router = require("express").Router()
+router.post("")
