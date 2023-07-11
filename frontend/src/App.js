1a727b33bd030f24041d9768d17f5cafd3c6245e
Kebaabetswe <keba@KebaabetswesMBP.home>
2023-07-11 19:26:53 +0200

    adding 4 pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Login from './components/Login';
+import Home from './components/Home';
+import SignUp from './components/Signup';
+import Services from './components/Services';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/services" element={<Services/>}/>
+    </Routes>
+   </BrowserRouter>
   );
 }
 
